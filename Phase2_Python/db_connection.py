975e35d1065acf1851c5b502ee6d07c618b2a58f
bladervnner <112827038+bladervnner@users.noreply.github.com>
2023-02-22 23:29:25 -0800

    Delete Phase2_Python directory

--- Phase2_Python/db_connection.py
+++
@@ -1,9 +0,0 @@
-#establishes connection with postgres database
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-
-db_url = "postgresql+psycopg2://postgres:<password>@localhost:5432/postgres"
-engine = create_engine(db_url, pool_size=5, pool_recycle=3600, echo=True)
-
-session_factory = sessionmaker(bind=engine)
-Session = scoped_session(session_factory)
--- Phase2_Python/orm_base.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.schema import MetaData
-
-Base = declarative_base(metadata=MetaData(schema="Hook_Project"))
-metadata = Base.metadata
